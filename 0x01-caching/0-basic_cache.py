eab38d8465f0d599d578ed06c86a7c39a06f5cb8
Damadet <adedokundamilare41@gmail.com>
2023-08-23 01:44:38 +0200

    $COMMIT

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,22 +1,35 @@
 #!/usr/bin/env python3
 '''
-BAsic dictionary caching
+Basic dictionary caching
 '''
 BaseCaching = __import__('base_caching').BaseCaching
 
 
+# class BasicCache(BaseCaching):
+#     '''Basic caching class'''
+
+#     def put(self, key, item):
+
+#         if key == None or item == None:
+#             return
+#         self.cache_data[key] = item
+
+#     def get(self, key):
+#         if key == None:
+#             return None
+#         if key not in self.cache_data.keys():
+#             return None
+#         return self.cache_data[key]
+
 class BasicCache(BaseCaching):
-    '''Basic caching class'''
+    ''' Basic Caching Class '''
 
     def put(self, key, item):
-
-        if key == None or item == None:
+        ''' put method for putting a new key and item '''
+        if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
-        if key == None:
-            return None
-        if key not in self.cache_data.keys():
-            return None
-        return self.cache_data[key]
+        ''' get method for retrieving item stored in key '''
+        return self.cache_data.get(key)
